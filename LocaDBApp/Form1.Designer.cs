63b36c000120dbc7bcba15aacc82972035615314
Anthony  Santana <anthony26uu@gmail.com>
2017-01-23 00:20:15 -0400

    Cambio

--- LocaDBApp/Form1.Designer.cs
+++ LocaDBApp/Form1.Designer.cs
@@ -29,11 +29,329 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label idLabel;
+            System.Windows.Forms.Label nombreLabel;
+            System.Windows.Forms.Label apellidoLabel;
+            System.Windows.Forms.Label edadLabel;
+            this.idMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.apellidoTextBox = new System.Windows.Forms.TextBox();
+            this.edadMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.informacionDataGridView = new System.Windows.Forms.DataGridView();
+            this.Estadolabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.buscarNombreToolStrip = new System.Windows.Forms.ToolStrip();
+            this.nameToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.nameToolStripTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.buscarNombreToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.informacionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dBDataSet = new LocaDBApp.DBDataSet();
+            this.informacionTableAdapter = new LocaDBApp.DBDataSetTableAdapters.informacionTableAdapter();
+            this.tableAdapterManager = new LocaDBApp.DBDataSetTableAdapters.TableAdapterManager();
+            idLabel = new System.Windows.Forms.Label();
+            nombreLabel = new System.Windows.Forms.Label();
+            apellidoLabel = new System.Windows.Forms.Label();
+            edadLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.informacionDataGridView)).BeginInit();
+            this.buscarNombreToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.informacionBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            idLabel.AutoSize = true;
+            idLabel.Location = new System.Drawing.Point(139, 33);
+            idLabel.Name = "idLabel";
+            idLabel.Size = new System.Drawing.Size(19, 13);
+            idLabel.TabIndex = 0;
+            idLabel.Text = "Id:";
+            // 
+            // idMaskedTextBox
+            // 
+            this.idMaskedTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.informacionBindingSource, "Id", true));
+            this.idMaskedTextBox.Location = new System.Drawing.Point(192, 30);
+            this.idMaskedTextBox.Name = "idMaskedTextBox";
+            this.idMaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idMaskedTextBox.TabIndex = 1;
+            // 
+            // nombreLabel
+            // 
+            nombreLabel.AutoSize = true;
+            nombreLabel.Location = new System.Drawing.Point(139, 78);
+            nombreLabel.Name = "nombreLabel";
+            nombreLabel.Size = new System.Drawing.Size(47, 13);
+            nombreLabel.TabIndex = 2;
+            nombreLabel.Text = "Nombre:";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.informacionBindingSource, "Nombre", true));
+            this.nombreTextBox.Location = new System.Drawing.Point(192, 75);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nombreTextBox.TabIndex = 3;
+            // 
+            // apellidoLabel
+            // 
+            apellidoLabel.AutoSize = true;
+            apellidoLabel.Location = new System.Drawing.Point(139, 139);
+            apellidoLabel.Name = "apellidoLabel";
+            apellidoLabel.Size = new System.Drawing.Size(47, 13);
+            apellidoLabel.TabIndex = 4;
+            apellidoLabel.Text = "Apellido:";
+            // 
+            // apellidoTextBox
+            // 
+            this.apellidoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.informacionBindingSource, "Apellido", true));
+            this.apellidoTextBox.Location = new System.Drawing.Point(192, 136);
+            this.apellidoTextBox.Name = "apellidoTextBox";
+            this.apellidoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.apellidoTextBox.TabIndex = 5;
+            // 
+            // edadLabel
+            // 
+            edadLabel.AutoSize = true;
+            edadLabel.Location = new System.Drawing.Point(139, 183);
+            edadLabel.Name = "edadLabel";
+            edadLabel.Size = new System.Drawing.Size(35, 13);
+            edadLabel.TabIndex = 6;
+            edadLabel.Text = "Edad:";
+            edadLabel.Click += new System.EventHandler(this.edadLabel_Click);
+            // 
+            // edadMaskedTextBox
+            // 
+            this.edadMaskedTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.informacionBindingSource, "Edad", true));
+            this.edadMaskedTextBox.Location = new System.Drawing.Point(192, 180);
+            this.edadMaskedTextBox.Name = "edadMaskedTextBox";
+            this.edadMaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.edadMaskedTextBox.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Nuevo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(217, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // informacionDataGridView
+            // 
+            this.informacionDataGridView.AutoGenerateColumns = false;
+            this.informacionDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.informacionDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.informacionDataGridView.DataSource = this.informacionBindingSource;
+            this.informacionDataGridView.Location = new System.Drawing.Point(307, 78);
+            this.informacionDataGridView.Name = "informacionDataGridView";
+            this.informacionDataGridView.Size = new System.Drawing.Size(482, 220);
+            this.informacionDataGridView.TabIndex = 11;
+            // 
+            // Estadolabel
+            // 
+            this.Estadolabel.AutoSize = true;
+            this.Estadolabel.Location = new System.Drawing.Point(515, 43);
+            this.Estadolabel.Name = "Estadolabel";
+            this.Estadolabel.Size = new System.Drawing.Size(40, 13);
+            this.Estadolabel.TabIndex = 12;
+            this.Estadolabel.Text = "Estado";
+            this.Estadolabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(651, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Siguiente";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(349, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Anterior";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buscarNombreToolStrip
+            // 
+            this.buscarNombreToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nameToolStripLabel,
+            this.nameToolStripTextBox,
+            this.buscarNombreToolStripButton});
+            this.buscarNombreToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.buscarNombreToolStrip.Name = "buscarNombreToolStrip";
+            this.buscarNombreToolStrip.Size = new System.Drawing.Size(831, 25);
+            this.buscarNombreToolStrip.TabIndex = 15;
+            this.buscarNombreToolStrip.Text = "buscarNombreToolStrip";
+            this.buscarNombreToolStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.buscarNombreToolStrip_ItemClicked);
+            // 
+            // nameToolStripLabel
+            // 
+            this.nameToolStripLabel.Name = "nameToolStripLabel";
+            this.nameToolStripLabel.Size = new System.Drawing.Size(51, 22);
+            this.nameToolStripLabel.Text = "Nombre";
+            // 
+            // nameToolStripTextBox
+            // 
+            this.nameToolStripTextBox.Name = "nameToolStripTextBox";
+            this.nameToolStripTextBox.Size = new System.Drawing.Size(100, 25);
+            // 
+            // buscarNombreToolStripButton
+            // 
+            this.buscarNombreToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.buscarNombreToolStripButton.Name = "buscarNombreToolStripButton";
+            this.buscarNombreToolStripButton.Size = new System.Drawing.Size(46, 22);
+            this.buscarNombreToolStripButton.Text = "Buscar";
+            this.buscarNombreToolStripButton.Click += new System.EventHandler(this.buscarNombreToolStripButton_Click);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Nombre";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nombre";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Apellido";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Apellido";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Edad";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Edad";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // informacionBindingSource
+            // 
+            this.informacionBindingSource.DataMember = "informacion";
+            this.informacionBindingSource.DataSource = this.dBDataSet;
+            // 
+            // dBDataSet
+            // 
+            this.dBDataSet.DataSetName = "DBDataSet";
+            this.dBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // informacionTableAdapter
+            // 
+            this.informacionTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.informacionTableAdapter = this.informacionTableAdapter;
+            this.tableAdapterManager.TableTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = LocaDBApp.DBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 415);
+            this.Controls.Add(this.buscarNombreToolStrip);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Estadolabel);
+            this.Controls.Add(this.informacionDataGridView);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(edadLabel);
+            this.Controls.Add(this.edadMaskedTextBox);
+            this.Controls.Add(apellidoLabel);
+            this.Controls.Add(this.apellidoTextBox);
+            this.Controls.Add(nombreLabel);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(idLabel);
+            this.Controls.Add(this.idMaskedTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.informacionDataGridView)).EndInit();
+            this.buscarNombreToolStrip.ResumeLayout(false);
+            this.buscarNombreToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.informacionBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DBDataSet dBDataSet;
+        private System.Windows.Forms.BindingSource informacionBindingSource;
+        private DBDataSetTableAdapters.informacionTableAdapter informacionTableAdapter;
+        private DBDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.MaskedTextBox idMaskedTextBox;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.TextBox apellidoTextBox;
+        private System.Windows.Forms.MaskedTextBox edadMaskedTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView informacionDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.Label Estadolabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolStrip buscarNombreToolStrip;
+        private System.Windows.Forms.ToolStripLabel nameToolStripLabel;
+        private System.Windows.Forms.ToolStripTextBox nameToolStripTextBox;
+        private System.Windows.Forms.ToolStripButton buscarNombreToolStripButton;
     }
 }
 
--- LocaDBApp/Form1.cs
+++ LocaDBApp/Form1.cs
@@ -15,5 +15,130 @@ namespace LocaDBApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dBDataSet.informacion' table. You can move, or remove it, as needed.
+            this.informacionTableAdapter.Fill(this.dBDataSet.informacion);
+            // TODO: This line of code loads data into the 'dBDataSet.informacion' table. You can move, or remove it, as needed.
+            this.informacionTableAdapter.Fill(this.dBDataSet.informacion);
+            timer1.Start();
+
+        }
+
+        private void informacionBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.informacionBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dBDataSet);
+
+        }
+
+        private void informacionBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.informacionBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dBDataSet);
+
+        }
+
+        private void informacionDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void edadLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void informacionDataGridView_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.informacionBindingSource.AddNew();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.informacionBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dBDataSet);
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.informacionBindingSource.RemoveCurrent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int contador = 0;
+
+        
+
+        //private object label1_status;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            contador = informacionBindingSource.Count;       
+
+            Estadolabel.Text = "Cantidad "+contador.ToString();
+
+            if(contador<2)
+            {
+                button4.Visible = false;
+                button5.Visible = false;
+
+            }
+            else
+            {
+                button4.Visible = true;
+                button5.Visible = true;
+            }
+            
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            informacionBindingSource.MoveNext();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            informacionBindingSource.MovePrevious();
+
+        }
+
+        private void buscarNombreToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.informacionTableAdapter.BuscarNombre(this.dBDataSet.informacion, nameToolStripTextBox.Text);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void buscarNombreToolStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
